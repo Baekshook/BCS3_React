c4be15d949c152f4b0e0e5ea064a2aaef214b3ff
Baekshook <ecgo0106@naver.com>
2023-03-30 15:45:47 +0900

    header setting

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,26 @@
+import Apple from "./images/logogo.gif";
+
 function App() {
   return (
-    <>
-      <div>
-        <h1 className="text-3xl font-bold underline">
-          Hello React!
-        </h1>
-        <h2 className="text-3xl font-bold underline">
-          My name is Eunchan
-        </h2>
-        <h3 className="text-3xl font-bold underline">
-          My Favorite drink is Zero Coke 🥤
-        </h3>
-      </div>
-    </>
+    <div className="bg-red-100 min-h-screen">
+      <header className="bg-blue-400 h-12 md:h-20">
+        <div className="max-w-screen-xl mx-auto h-full flex justify-between items-center">
+          {/* <div>Eunchan's Portfolio</div> */}
+          <div>
+            {" "}
+            <img src={Apple} alt="logo" className="w-24" />
+          </div>
+          <ul className="flex gap-2 md:gap-8 text-xs md:text-base">
+            <li className="bg-indigo-300 btn-style">Introduce</li>
+            <li className="bg-purple-300 btn-style">Portfolio</li>
+            <li className="bg-blue-300 btn-style">Contact Me</li>
+          </ul>
+        </div>
+      </header>
+    </div>
   );
 }
 
 export default App;
+
+// max-w-screen-xl 1200px까지만 늘어나게, mx-auto 자동 정렬, h-full 높이가 부모랑 똑같아짐
