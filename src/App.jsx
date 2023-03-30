188c96116f61b5dea935b2809124e5b8de870e43
Baekshook <ecgo0106@naver.com>
2023-03-30 11:49:28 +0900

    tailwind setting

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,15 @@ function App() {
   return (
     <>
       <div>
-        <h1>Hello React!</h1>
-        <h2>My name is Eunchan</h2>
+        <h1 className="text-3xl font-bold underline">
+          Hello React!
+        </h1>
+        <h2 className="text-3xl font-bold underline">
+          My name is Eunchan
+        </h2>
+        <h3 className="text-3xl font-bold underline">
+          My Favorite drink is Zero Coke 🥤
+        </h3>
       </div>
     </>
   );
