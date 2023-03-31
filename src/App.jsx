cbf41f443d7a38d8daf597be4ea899dbac72146e
Baekshook <ecgo0106@naver.com>
2023-03-31 09:13:45 +0900

    footer setting

--- src/App.jsx
+++ src/App.jsx
@@ -75,6 +75,9 @@ function App() {
           내 연락처
         </div>
       </main>
+      <footer className="max-w-screen-xl mx-auto flex justify-end py-1 md:py-2 ">
+        2023 Designed by, Blackcons
+      </footer>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        main : {
+          100: "#f1c40f",
+          200: "#07bc0c",
+          300: "#f19489",
+        }
+      }
+    },
   },
   plugins: [],
 };
