302991c0013cae16a8efd4f96f121c9a78a87c32
Baekshook <ecgo0106@naver.com>
2023-03-31 14:54:56 +0900

    App.jsx 분리

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,11 @@
-import Header from "@components/Header";
-import Main from "@components/Main";
-import Footer from "@components/Footer";
+import MyPortfolio from "@components/MyPortfolio";
 
 function App() {
   return (
     <>
-      <div className="bg-red-100 min-h-screen">
-        <Header />
-        <Main />
-        <Footer />
-      </div>
+      <MyPortfolio />
     </>
   );
 }
 
 export default App;
-
-// max-w-screen-xl 1200px까지만 늘어나게, mx-auto 자동 정렬, h-full 높이가 부모랑 똑같아짐
---
+++ src/components/MyPortfolio.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "@components/organism/Header";
+import Main from "@components/organism/Main";
+import Footer from "@components/organism/Footer";
+
+export default function MyPortfolio() {
+  return (
+    <div className="bg-red-100 min-h-screen">
+      <Header />
+      <Main />
+      <Footer />
+    </div>
+  );
+}
