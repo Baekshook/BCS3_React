718842f248f5596ac4242ecd39086315c131937e
Baekshook <ecgo0106@naver.com>
2023-04-04 09:15:03 +0900

    card props, map

--- src/components/atoms/ProjectName.jsx
+++ src/components/atoms/ProjectName.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 
-export default function ProjectName() {
+export default function ProjectName(props) {
   return (
     <li className="bg-purple-300 w-60 h-72 rounded-xl">
       {/* object-fit: cover */}
       <div className="bg-yellow-100 w-full h-1/2 rounded-t-xl">
-        프로젝트 이미지
+        {props.images}
       </div>
-      <div className="text-xl font-bold mt-2 mx-2">프로젝트 제목</div>
-      <div className="mt-2 mx-2">프로젝트 간략한 설명</div>
+      <div className="text-xl font-bold mt-2 mx-2">{props.content}</div>
+      <div className="mt-2 mx-2">{props.description}</div>
     </li>
   );
 }
--- src/components/molcules/UlProject.jsx
+++ src/components/molcules/UlProject.jsx
@@ -1,15 +1,20 @@
 import React from "react";
 import ProjectName from "../atoms/ProjectName";
+import projectContents from "../../projectContents.json";
 
 export default function UlProject() {
   return (
     <ul className="bg-indigo-100 max-w-screen-xl mx-auto grid grid-cols-2 justify-items-center gap-8 py-8">
-      <ProjectName />
-      <ProjectName />
-      <ProjectName />
-      <ProjectName />
-      <ProjectName />
-      <ProjectName />
+      {projectContents.map((v, i) => {
+        return (
+          <ProjectName
+            key={i}
+            images={v.images}
+            content={v.content}
+            description={v.description}
+          />
+        );
+      })}
     </ul>
   );
 }
