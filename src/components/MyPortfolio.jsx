60969eb720c595e6f520a51e5737476aa9748b53
Baekshook <ecgo0106@naver.com>
2023-04-23 19:29:30 +0900

    Router 적용

--- src/components/MyPortfolio.jsx
+++ src/components/MyPortfolio.jsx
@@ -2,13 +2,18 @@ import React from "react";
 import Header from "@components/organism/Header";
 import Main from "@components/organism/Main";
 import Footer from "@components/organism/Footer";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 export default function MyPortfolio() {
   return (
-    <div className="bg-red-100 min-h-screen">
-      <Header />
-      <Main />
-      <Footer />
-    </div>
+    <BrowserRouter>
+      <div className="bg-red-100 min-h-screen">
+        <Header />
+        <Routes>
+          <Route path="/" element={<Main />} />
+        </Routes>
+        <Footer />
+      </div>
+    </BrowserRouter>
   );
 }
