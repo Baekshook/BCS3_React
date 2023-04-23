5254e6467c6fdf1b99abc895c9a5914696e475d6
Baekshook <ecgo0106@naver.com>
2023-04-23 19:35:02 +0900

    Main 페이지 버튼 추가, Portfolio 페이지 footer 공간 확보

--- src/components/atoms/ProjectName.jsx
+++ src/components/atoms/ProjectName.jsx
@@ -1,8 +1,8 @@
-import React from 'react'
+import React from "react";
 
 export default function ProjectName({ title, desc, image }) {
   return (
-    <li className="bg-purple-300 w-60 h-72 rounded-xl">
+    <li className="bg-purple-300 w-60 h-72 rounded-xl relative">
       {/* object-fit: cover */}
       <div className="bg-yellow-100 w-full h-1/2 rounded-t-xl">
         <img
@@ -13,6 +13,9 @@ export default function ProjectName({ title, desc, image }) {
       </div>
       <div className="text-xl font-bold mt-2 mx-2">{title}</div>
       <div className="mt-2 mx-2">{desc}</div>
+      <button className=" absolute bottom-0 right-0 m-4 px-4 py-2 rounded-full bg-red-500">
+        View
+      </button>
     </li>
   );
 }
--- src/components/organism/Footer.jsx
+++ src/components/organism/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Footer() {
   return (
-    <footer className="max-w-screen-xl mx-auto flex justify-end py-1 md:py-2 ">
+    <footer className="max-w-screen-xl mx-auto w-full grow flex flex-col justify-center items-center py-8">
       2023 Designed by, Blackcons
     </footer>
   );
